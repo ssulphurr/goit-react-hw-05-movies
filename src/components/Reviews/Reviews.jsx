92eb1c8cfb17242458f8b1bca2ab726b5918d777
ssulphurr <ssulphurr@gmail.com>
2023-06-03 18:06:32 +0300

    adds Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function Reviews() {
-  return <div>reviews</div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=453b881a671dd013b145c543ca73b9df`
+      );
+
+      const data = await response.json();
+      setReviews(data.results);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {reviews.map(({ id, author, content }) => (
+          <li key={id}>
+            <div>
+              <h3>Author: {author}</h3>
+              <p>{content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
